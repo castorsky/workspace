572a1fed9998144c6206675e3e899864493e6a3b
castorsky <csky57@gmail.com>
2013-10-09 11:55:36 +0900

    Some changes

--- user-create/main.pl
+++ user-create/main.pl
@@ -1,2 +1,9 @@
 #!/usr/bin/env perl
 
+use OpenOffice::OODoc;
+
+my $document = odfDocument(file => 'user-list.ods');
+my $element = $document->getElement('//text:p', 2);
+my $text = $document->getText($element);
+
+print $text;
