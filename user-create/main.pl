c95b67f89d9aacee4fe84771b207aec6ec72395c
Castor Sky <csky57@gmail.com>
2013-10-20 14:45:16 +0900

    Reading of names correct

--- user-create/main.pl
+++ user-create/main.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/env perl
 
-#use Encode;
 use OpenOffice::OODoc;
 use Lingua::Translit;
 
@@ -8,9 +7,19 @@ my $columns =  5;
 my $document = odfDocument(file => 'user-list.ods');
 my $rows = $document->getTableRows('userlist');
 my $table = $document->getTable('userlist', $columns, $rows);
+$transl = new Lingua::Translit("GOST 7.79 RUS");
 
 for ($row = 1; $row < $rows; $row++) {
-	my $testcell = $document->getCellValue($table, $row, 0);
-	$testcell =~ s/^(\S+)\s+(\S+)(\s|\.)(\S+).*/$4/;
-	if ($testcell) { print $testcell."\n"; };
+	my $FIO = $document->getCellValue($table, $row, 0);
+	next if !$FIO;
+	$Name = $transl->translit($FIO);
+	$Name =~ s/`//g;
+	$Name =~ m/^[A-Za-z]+(?=\s.*)/;
+	my $LastName = $&;
+	$Name =~ m/^[A-Za-z]+\s\K[A-Za-z]+(?=(\s|\.).*)/;
+	my $FirstName = $&;
+	$Name =~ m/^[A-Za-z]+\s[A-Za-z]+(\s|\.)\s*\K[A-Za-z]+(?=.*)/;
+	my $Patronimic = $&;
+	
+	print $Name."\n".$LastName."\n".$FirstName."\n".$Patronimic."\n";
 }
