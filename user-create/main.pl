1bf4e725830843e692db6786e5b9b316897a0b01
Castor Sky <csky57@gmail.com>
2013-10-20 15:38:37 +0900

    ODF-module do not want to read 4th column

--- user-create/main.pl
+++ user-create/main.pl
@@ -3,23 +3,43 @@
 use OpenOffice::OODoc;
 use Lingua::Translit;
 
-my $columns =  5;
+my $columns =  6;
 my $document = odfDocument(file => 'user-list.ods');
 my $rows = $document->getTableRows('userlist');
 my $table = $document->getTable('userlist', $columns, $rows);
 $transl = new Lingua::Translit("GOST 7.79 RUS");
 
+# Sub was taken at http://perl.about.com/od/perltutorials/a/genpassword.htm
+# Slightly modified - $password is local variable now.
+sub generatePassword {
+   $length = shift;
+   my $possible = 'abcdefghijkmnpqrstuvwxyz23456789';
+   my $password = ''; 
+   while (length($password) < $length) {
+     $password .= substr($possible, (int(rand(length($possible)))), 1);
+   }
+   return $password
+ } 
+
+print $rows."\n";
+
 for ($row = 1; $row < $rows; $row++) {
-	my $FIO = $document->getCellValue($table, $row, 0);
-	next if !$FIO;
-	$Name = $transl->translit($FIO);
-	$Name =~ s/`//g;
-	$Name =~ m/^[A-Za-z]+(?=\s.*)/;
-	my $LastName = $&;
-	$Name =~ m/^[A-Za-z]+\s\K[A-Za-z]+(?=(\s|\.).*)/;
-	my $FirstName = $&;
-	$Name =~ m/^[A-Za-z]+\s[A-Za-z]+(\s|\.)\s*\K[A-Za-z]+(?=.*)/;
-	my $Patronimic = $&;
-	
-	print $Name."\n".$LastName."\n".$FirstName."\n".$Patronimic."\n";
+	print $document->getCellValue($table, $row, 3)."\n";
 }
+
+#for ($row = 1; $row < $rows; $row++) {
+#	my $FIO = $document->getCellValue($table, $row, 0);
+#	my $groupList = $document->getCellValue($table, $row, 3);
+#	next if !$FIO;
+#	$Name = $transl->translit($FIO);
+#	$Name =~ s/`//g;
+#	$Name =~ m/^[A-Za-z]+(?=\s.*)/;
+#	my $LastName = $&;
+#	$Name =~ m/^[A-Za-z]+\s\K[A-Z](?=[a-z]*(\s|\.).*)/;
+#	my $FirstName = $&;
+#	$Name =~ m/^[A-Za-z]+\s[A-Za-z]+(\s|\.)\s*\K[A-Z](?=[a-z]*.*)/;
+#	my $Patronimic = $&;
+#	my $Initials = $LastName.$FirstName.$Patronimic;
+#	$tempPasswd = generatePassword(8);
+#	print $Initials." - ".$tempPasswd."\n".$groupList."\n";
+#}
