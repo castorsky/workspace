12ef96c7764886c598d15b872f5e0d95a2879894
castorsky <csky57@gmail.com>
2013-10-21 11:30:39 +0900

    Near final version

--- user-create/main.pl
+++ user-create/main.pl
@@ -9,9 +9,9 @@ my $rows = $document->getTableRows('userlist');
 my $table = $document->getTable('userlist', $columns, $rows);
 $transl = new Lingua::Translit("GOST 7.79 RUS");
 
+sub generatePassword {
 # Sub was taken at http://perl.about.com/od/perltutorials/a/genpassword.htm
 # Slightly modified - $password is local variable now.
-sub generatePassword {
    $length = shift;
    my $possible = 'abcdefghijkmnpqrstuvwxyz23456789';
    my $password = ''; 
@@ -21,25 +21,28 @@ sub generatePassword {
    return $password
  } 
 
-print $rows."\n";
-
 for ($row = 1; $row < $rows; $row++) {
-	print $document->getCellValue($table, $row, 3)."\n";
+	my $FIO = $document->getCellValue($table, $row, 0);
+	next if !$FIO;
+	$Name = $transl->translit($FIO);
+	# Some cyrillic letters appears as ` or y'
+	$Name =~ s/[`']//g;
+	$Name =~ m/^[A-Za-z]+(?=\s.*)/;
+	my $LastName = $&;
+	$Name =~ m/^[A-Za-z]+\s*\K[A-Z](?=[a-z]*(\s|\.).*)/;
+	my $FirstName = $&;
+	$Name =~ m/^[A-Za-z]+\s*[A-Za-z]+(\s|\.)\s*\K[A-Z](?=[a-z]*.*)/;
+	my $Patronimic = $&;
+	my $Initials = $LastName.$FirstName.$Patronimic;
+	$tempPasswd = generatePassword(8);
+	$document->cellValueType($table, $row, 1, 'string');
+	$document->cellValue($table, $row, 1, $Initials);
+	$document->cellValueType($table, $row, 2, 'string');
+	$document->cellValue($table, $row, 2, $tempPasswd);
+	my $groupList = $document->getCellValue($table, $row, 3);
+	my $command = 'cl-useradd -c "'.$FIO.'" -g users -G "'.$groupList.'" -P	'.$Initials.' samba <<< "'.$tempPasswd.'"';
+	print $Initials." - ".$tempPasswd." - ".$groupList."\n";
+	print $command."\n";
 }
 
-#for ($row = 1; $row < $rows; $row++) {
-#	my $FIO = $document->getCellValue($table, $row, 0);
-#	my $groupList = $document->getCellValue($table, $row, 3);
-#	next if !$FIO;
-#	$Name = $transl->translit($FIO);
-#	$Name =~ s/`//g;
-#	$Name =~ m/^[A-Za-z]+(?=\s.*)/;
-#	my $LastName = $&;
-#	$Name =~ m/^[A-Za-z]+\s\K[A-Z](?=[a-z]*(\s|\.).*)/;
-#	my $FirstName = $&;
-#	$Name =~ m/^[A-Za-z]+\s[A-Za-z]+(\s|\.)\s*\K[A-Z](?=[a-z]*.*)/;
-#	my $Patronimic = $&;
-#	my $Initials = $LastName.$FirstName.$Patronimic;
-#	$tempPasswd = generatePassword(8);
-#	print $Initials." - ".$tempPasswd."\n".$groupList."\n";
-#}
+$document->save;
