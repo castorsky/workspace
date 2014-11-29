683a5f0369d805607b7ae2f6163c82e557abee57
Castor <castor@twilight.localdomain>
2014-11-29 12:06:08 +0800

    Add EPEL and ELREPO repositories

--- centos7localmirror.sh
+++ centos7localmirror.sh
@@ -4,3 +4,13 @@ if [ -d /data/localmirror/centos/7.0.1406 ] ; then
 else
     echo "Target directory /data/localmirror/centos/7.0.1406 not present."
 fi
+if [ -d /data/localmirror/elrepo ] ; then
+    rsync  -avSHP --delete --exclude "*/el5/*" --exclude "*/el6/*" --exclude "*/i386/*" elrepo.reloumirrors.net::elrepo/ /data/localmirror/elrepo/
+else
+    echo "Target directory /data/localmirror/elrepo not present."
+fi
+if [ -d /data/localmirror/epel/7/x86_64 ] ; then
+    rsync  -avSHP --delete mirror.logol.ru::epel/7/x86_64/ /data/localmirror/epel/7/x86_64/
+else
+    echo "Target directory /data/localmirror/epel/7/x86_64 not present."
+fi
