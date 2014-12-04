1f04da2abc3be63cd44958948d4130395309b145
Castor <castor@twilight.localdomain>
2014-12-04 22:29:16 +0800

    Some changes

--- centos7localmirror.sh
+++ centos7localmirror.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-if [ -d /data/localmirror/centos/7.0.1406 ] ; then
-    rsync  -avSHP --delete --exclude "local*" --exclude "isos" mirror.yandex.ru::centos/7.0.1406/ /data/localmirror/centos/7.0.1406/
+CENTOS_RELEASE="6.6"
+if [ -d /data/localmirror/centos/${CENTOS_RELEASE} ] ; then
+    rsync  -avSHP --delete --exclude "SCL" --exclude "cr" --exclude "xen4" --exclude "isos" mirror.yandex.ru::centos/${CENTOS_RELEASE}/ /data/localmirror/centos/${CENTOS_RELEASE}/
 else
-    echo "Target directory /data/localmirror/centos/7.0.1406 not present."
+    echo "Target directory /data/localmirror/centos/${CENTOS_RELEASE} not present."
 fi
 if [ -d /data/localmirror/elrepo ] ; then
-    rsync  -avSHP --delete --exclude "*/el5/*" --exclude "*/el6/*" --exclude "*/i386/*" elrepo.reloumirrors.net::elrepo/ /data/localmirror/elrepo/
+    rsync  -avSHP --delete --exclude "*/el5/*" --exclude "*/el7/*" elrepo.reloumirrors.net::elrepo/ /data/localmirror/elrepo/
 else
     echo "Target directory /data/localmirror/elrepo not present."
 fi
-if [ -d /data/localmirror/epel/7/x86_64 ] ; then
-    rsync  -avSHP --delete mirror.logol.ru::epel/7/x86_64/ /data/localmirror/epel/7/x86_64/
+if [ -d /data/localmirror/epel/6 ] ; then
+    rsync  -avSHP --delete mirror.logol.ru::epel/6/ /data/localmirror/epel/6/
 else
-    echo "Target directory /data/localmirror/epel/7/x86_64 not present."
+    echo "Target directory /data/localmirror/epel/6 not present."
 fi
