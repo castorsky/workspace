b4b35fb63c1546102d5869d7b31e0ce73c3bc3dc
castorsky <csky57@gmail.com>
2014-12-05 13:49:13 +0800

    Update sync_centos6_mirror.sh

--- sync_centos6_mirror.sh
+++ sync_centos6_mirror.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CENTOS_RELEASE="6.6"
+ELREPO_EXCLUDE_RELEASE="7"
+EPEL_RELEASE="6"
 BASE_DIR="/var/calculate/server-data/samba/share/soft/localmirror"
 
 if [ -d ${BASE_DIR}/centos/${CENTOS_RELEASE} ] ; then
@@ -9,13 +11,13 @@ else
 fi
 
 if [ -d ${BASE_DIR}/elrepo ] ; then
-  rsync -avSHP --delete --exclude "*/el5/*" --exclude "*/el7/*" elrepo.reloumirrors.net::elrepo/ ${BASE_DIR}/elrepo/
+  rsync -avSHP --delete --exclude "*/el5/*" --exclude "*/el${ELREPO_EXCLUDE_RELEASE}/*" elrepo.reloumirrors.net::elrepo/ ${BASE_DIR}/elrepo/
 else
   echo "Target directory ${BASE_DIR}/elrepo not present."
 fi
 
 if [ -d ${BASE_DIR}/epel/6 ] ; then
-  rsync -avSHP --delete --exclude "SRPMS" --exclude "ppc64" --exclude "*/debug/*" mirror.logol.ru::epel/6/ ${BASE_DIR}/epel/6/
+  rsync -avSHP --delete --exclude "SRPMS" --exclude "ppc64" --exclude "*/debug/*" mirror.logol.ru::epel/${EPEL_RELEASE}/ ${BASE_DIR}/epel/${EPEL_RELEASE}/
 else
-  echo "Target directory ${BASE_DIR}/epel/6 not present."
+  echo "Target directory ${BASE_DIR}/epel/${EPEL_RELEASE} not present."
 fi
