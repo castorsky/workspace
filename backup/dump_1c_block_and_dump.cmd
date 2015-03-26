cdb86072121fdb054218148fffc0279744607d96
castorsky <csky57@gmail.com>
2015-03-26 18:37:16 +0800

    Update dump_1c_block_and_dump.cmd

--- backup/dump_1c_block_and_dump.cmd
+++ backup/dump_1c_block_and_dump.cmd
@@ -1,5 +1,5 @@
 @ECHO  OFF
-SET FOLDER="D:\1C_BACKUP"
+SET FOLDER=D:\1C_BACKUP
 SET BASE="D:\1C_DATA\MM"
 SET BASENAME="SmallBusiness"
 SET USER="backup_user"
@@ -17,10 +17,10 @@ FOR /F "tokens=1-2 delims=/:" %%D IN ("%TIME%") DO (
 	SET HOUR=%%D
 	SET MINUTE=%%E
 )
+SET FILENAME=%FOLDER%\MMbase_%YEAR%%MONTH%%DAY%_%HOUR%%MINUTE%.dt
 rem Запретить пользователям работать в базе
 %PROG% ENTERPRISE /F %BASE% /N %USER% /P %PWD% /C %DENY% /DisableStartupMessages
 rem Спать 300 секунд
 ping -n 120 127.0.0.1 >nul 2>&1
 rem Собственно запуск выгрузки базы
-CD %FOLDER%
-%PROG% DESIGNER /IBName %BASENAME% /N %USER% /P %PWD% /DumpIB "%YEAR%%MONTH%%DAY%_%HOUR%%MINUTE%.dt" /UC %ALLOWCODE% /DisableStartupMessages
+%PROG% DESIGNER /IBName %BASENAME% /N %USER% /P %PWD% /DumpIB %FILENAME% /UC %ALLOWCODE% /DisableStartupMessages
