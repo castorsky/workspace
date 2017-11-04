6fd6b1033bee6a08d69286b0f464691364e6096b
castor <csky57@gmail.com>
2017-11-05 00:45:02 +0800

    MegaCMD script added

---
+++ backup/mega/mega.ps1
@@ -0,0 +1,26 @@
+Param(
+   [string]$localSource,
+   [string]$targetName,
+   [int]$daysToExpire
+)
+$env:PATH +=";$env:LOCALAPPDATA\MEGAcmd"
+$currentDate = Get-Date -UFormat "%Y_%m_%d"
+$expireDate = Get-Date -Date $(Get-Date).AddDays(-$daysToExpire) -UFormat "%Y_%m_%d"
+
+mega-login arnistudio@ya.ru 4LUC3y67nc7S2kkephyI
+mega-cd backup/$targetName
+$backupList = mega-ls
+Foreach ($date in $backupList) {
+   If ($date -lt $expireDate) {
+      mega-rm -rf $date
+   }
+}
+mega-mkdir $currentDate
+cd $localSource
+$localFiles = Get-ChildItem -Name
+Foreach ($item in $localFIles) {
+   mega-put $item $currentDate
+}
+
+mega-logout
+mega-exit
