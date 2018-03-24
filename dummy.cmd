1e2dc2a7b23dcdb452ad7f30ee3eba669e8cb3ae
Castor <castor@duralex.localdomain>
2018-03-24 19:22:23 +0800

    Added dummy

---
+++ dummy.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF
+ECHO Ž„Ž†„ˆ’… €“ Œˆ“’ ……„ ‡€“‘ŠŽŒ Žƒ€ŒŒ›
+FOR /l %%a in (120,-1,1) do (TITLE %title% -- ŽŠŽ ‘€ŒŽ ‡€ŠŽ…’‘Ÿ —……‡ %%a c&ping -n 2 -w 1 127.0.0.1>NUL&echo|set /p=.)
