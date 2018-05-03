200b82514c9247d75c2315afe6a4126a810eb13e
Михаил Абрамов <castor@spec03.safetek.co>
2018-05-03 11:56:08 +0800

    New login

--- backup/mega/mega.ps1
+++ backup/mega/mega.ps1
@@ -7,7 +7,7 @@ $env:PATH +=";$env:LOCALAPPDATA\MEGAcmd"
 $currentDate = Get-Date -UFormat "%Y_%m_%d"
 $expireDate = Get-Date -Date $(Get-Date).AddDays(-$daysToExpire) -UFormat "%Y_%m_%d"
 
-mega-login arnistudio@ya.ru 4LUC3y67nc7S2kkephyI
+mega-login studio@ya.ru 4LUC3y67nc7S2kkephyI
 mega-cd backup/$targetName
 $backupList = mega-ls
 Foreach ($date in $backupList) {
