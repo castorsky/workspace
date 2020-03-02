d6d60b7e3ce597c8b5c07012391a982e7f716606
root <root@1cbo.safetek.co>
2020-03-02 17:24:51 +0800

    Double check PATH before adding CryptoPro path

--- terminal_server/etc/profile.d/cryptopath.sh
+++ terminal_server/etc/profile.d/cryptopath.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 CPBIN=/opt/cprocsp/bin/amd64
 CPSBIN=/opt/cprocsp/sbin/amd64
-if [ -d "$CPBIN" ]; then
+if [ -d $CPBIN ] && ( ! echo ${PATH} | /bin/grep -q $CPBIN ) ; then
     PATH=$PATH:$CPBIN
 fi
-if [ -d "$CPSBIN" ]; then
+if [ -d "$CPSBIN" ] && ( ! echo ${PATH} | /bin/grep -q $CPSBIN ) ; then
     PATH=$PATH:$CPSBIN
 fi
 export PATH
